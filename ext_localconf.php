87e543f4548c14a2494e3d69fe4e5d074abfbb86
Andriy Oprysko <pyvasyk@gmail.com>
2019-11-12 15:40:02 +0200

    [FEATURE] register TableGarbageCollectionTask table

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,3 +2,15 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pxa_form_enhancement/Configuration/TypoScript/PageTS/FormWizard.ts">');
+
+call_user_func(function () {
+    // Scheduler clean up of saved forms
+    $garbageTask = \TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class;
+    $table = 'tx_pxaformenhancement_domain_model_form';
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][$garbageTask]['options']['tables'][$table])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][$garbageTask]['options']['tables'][$table] = [
+            'dateField' => 'crdate',
+            'expirePeriod' => 30,
+        ];
+    }
+});
