751bc452151c6e08cf5232ca29ffb40bb76afab1
Andriy Oprysko <pyvasyk@gmail.com>
2019-11-12 15:22:30 +0200

    [FEATURE] register TableGarbageCollectionTask table

---
+++ ext_localconf.php
@@ -0,0 +1,14 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(function () {
+    // Scheduler clean up of saved forms
+    $garbageTask = \TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class;
+    $table = 'tx_pxaformenhancement_domain_model_form';
+    if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][$garbageTask]['options']['tables'][$table])) {
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][$garbageTask]['options']['tables'][$table] = [
+            'dateField' => 'crdate',
+            'expirePeriod' => 30,
+        ];
+    }
+});
