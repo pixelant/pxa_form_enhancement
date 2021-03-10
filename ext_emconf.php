90ae42a668036e2714ea9a596c2399af87b35963
Ihor Kulmatytskyy <ihor.kulmatytskyy@resultify.se>
2021-03-10 16:03:51 +0200

    [TASK] add compatibility with TYPO3 v10

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.2.7',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.5.99',
+            'typo3' => '8.7.0-10.4.99',
         ],
         'conflicts' => [
         ],
