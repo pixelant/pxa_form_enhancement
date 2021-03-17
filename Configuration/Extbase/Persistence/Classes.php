a035a0bb1fa3acf77f1efdbba196b729b7956fa2
ihorko <ikulmatytskyy@gmail.com>
2021-03-17 11:13:12 +0200

    [FEATURE] Improved v10 support and coding style improvements
    Also merges #29, thanks to @raphaeltsingos

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types = 1);
+
+return [
+	\Pixelant\PxaFormEnhancement\Domain\Model\FileReference::class => [
+		'tableName' => 'sys_file_reference',
+	],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -11,4 +11,37 @@ call_user_func(function () {
             'expirePeriod' => 30,
         ];
     }
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
+module.tx_form {
+    settings {
+        yamlConfigurations {
+            510 = EXT:pxa_form_enhancement/Configuration/Yaml/PxaBaseSetup.yaml
+            520 = EXT:pxa_form_enhancement/Configuration/Yaml/PxaFormEditor.yaml
+            530 = EXT:pxa_form_enhancement/Configuration/Yaml/PxaFormEngine.yaml
+        }
+    }
+}'
+    ));
+
+    // supporting TYPO3 v8 and v9
+    $typoVersionArray = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionStringToArray(
+        \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version()
+    );
+
+    if (in_array($typoVersionArray['version_main'], [8, 9])) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(trim('
+config.tx_extbase {
+    persistence {
+        classes {
+            Pixelant\PxaFormEnhancement\Domain\Model\FileReference {
+                mapping {
+                    tableName = sys_file_reference
+                }
+            }
+        }
+    }
+}'
+        ));
+    }
 });
