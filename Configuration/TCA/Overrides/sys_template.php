c5d13fc8b8a8ca76ef59293b418bc8bfcb73efd7
Mathias Bolt Lesniak <mathias@pixelant.no>
2021-09-24 11:42:35 +0200

    [BUGFIX] Separate TypoScript by T3 version (#32)
    Fixes #31

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,8 +1,25 @@
 <?php
 defined('TYPO3_MODE') or die();
 
+// supporting TYPO3 v8 and v9
+$majorTypo3Version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionStringToArray(
+    \TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version()
+)['version_main'];
+
+switch ($majorTypo3Version) {
+    case 8:
+        $templatePathPostfix = '/Typo3v8Compatibility';
+        break;
+    case 9:
+        $templatePathPostfix = '/Typo3v9Compatibility';
+        break;
+    default:
+        $templatePathPostfix = '';
+        break;
+}
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
     'pxa_form_enhancement',
-    'Configuration/TypoScript',
+    'Configuration/TypoScript' . $templatePathPostfix,
     'Pxa Form Enhancement'
 );
